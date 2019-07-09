e8b02c97a70ecc0e1c6fb01d1f31954a8ac0cb46
cc <cc--x@163.com>
2019-07-09 15:21:05 +0800

    - 增加主题选择器

--- web/src/baseComponent/CcThemePicker.vue
+++ web/src/baseComponent/CcThemePicker.vue
@@ -1,6 +1,6 @@
 <template>
   <el-color-picker v-model="theme"
-                   :predefine="['#409EFF', '#304156','#212121','#11a983', '#13c2c2', '#6959CD', '#f5222d', ]"
+                   :predefine="['#3c8dbc', '#304156','#212121','#11a983', '#13c2c2', '#6959CD', '#f5222d', ]"
                    class="theme-picker"
                    popper-class="theme-picker-dropdown" />
 </template>
